f10fe8453b14317578061d9563b4bf3be8dbc6a1
Filip Morten Lund Andersen <fili0360@edu.easj.dk>
2017-01-31 10:06:54 +0100

    Converter til DateTime Offset added

---
+++ EventMakerOpgave/Converter/DateTimeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMakerOpgave.Converter
+{
+    class DateTimeConverter
+    {
+
+
+        public static DateTime DateTimeOffsetAndTimeSetToDateTime(DateTimeOffset date, TimeSpan time)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, 0);
+        }
+    }
+}
--- EventMakerOpgave/ViewModel/EventViewModel.cs
+++ EventMakerOpgave/ViewModel/EventViewModel.cs
@@ -17,14 +17,57 @@ namespace EventMakerOpgave.ViewModel
             set { eventCollection = value; }
         }
 
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
 
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
 
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private string place;
+        public string Place
+        {
+            get { return place; }
+            set { place = value; }
+        }
+
+        private DateTimeOffset date;
+        public DateTimeOffset Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private TimeSpan time;
+        public TimeSpan Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
 
 
         public EventViewModel()
         {
             EventCollection = new ObservableCollection<Event>();
-            EventCollection = EventCatalogSingleton.Instance.EventCollection;    
+            EventCollection = EventCatalogSingleton.Instance.EventCollection;
+
+            DateTime dt = System.DateTime.Now; 
+            date = new DateTimeOffset(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0, new TimeSpan());
+            time = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
 
         }
 
