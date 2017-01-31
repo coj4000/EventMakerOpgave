93062bb40fe250a2c2207b28d94db9cfc8866ad0
Filip Morten Lund Andersen <fili0360@edu.easj.dk>
2017-01-31 10:51:28 +0100

    Added eventhandler til handler folder.

---
+++ EventMakerOpgave/Handler/EventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventMakerOpgave.ViewModel;
+using EventMakerOpgave.Model;
+using EventMakerOpgave.Converter;
+
+namespace EventMakerOpgave.Handler
+{
+
+    class EventHandler
+    {
+
+        public EventViewModel EventViewModel { get; set; }
+
+        public EventHandler(EventViewModel evm)
+        {
+            this.EventViewModel = evm;
+        }
+
+        public void CreateEvent()
+        {
+            EventCatalogSingleton.Instance.AddEvent(new Event(EventViewModel.Id, EventViewModel.Name, EventViewModel.Description, EventViewModel.Place, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(EventViewModel.Date, EventViewModel.Time)));
+        }
+
+    }
+}
