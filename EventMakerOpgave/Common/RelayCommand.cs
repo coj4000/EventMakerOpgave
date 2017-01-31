8bcaeedde7225d4b260008f6f28971e66fc86f25
Filip Morten Lund Andersen <fili0360@edu.easj.dk>
2017-01-31 10:59:52 +0100

    Implementeret ICommand interface.

---
+++ EventMakerOpgave/Common/RelayCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace EventMakerOpgave.Common
+{
+    class RelayCommand : ICommand
+    {
+        private Action _execute;
+        private Func<bool> _canExecute;
+        private DispatcherTimer canExecuteChangedEventTimer;
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+
+            this.canExecuteChangedEventTimer = new DispatcherTimer();
+            this.canExecuteChangedEventTimer.Tick += canExecuteChangedEventTimer_Tick;
+            this.canExecuteChangedEventTimer.Interval = new TimeSpan(0, 0, 1);
+            this.canExecuteChangedEventTimer.Start();
+        }
+
+        private void canExecuteChangedEventTimer_Tick(object sender, object e)
+        {
+            this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
